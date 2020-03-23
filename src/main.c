e192410544e5352115564798de9e841485de40b6
Lucas Schneider <lucastrschneider@gmail.com>
2020-03-22 23:59:38 -0300

    Add source code

--- src/main.c
+++ src/main.c
@@ -5,12 +5,16 @@
  */
 
 #include "mcu.h"
+#include "buzzer.h"
+#include "adc.h"
+#include "utils.h"
 
 /*****************************************
  * Private Constant Definitions
  *****************************************/
 
-#define LED_TOGGLE_DELAY_MS 1500
+#define MIN_FREQUENCY_HZ 1000
+#define MAX_FREQUENCY_HZ 5000
 
 /*****************************************
  * Main Function
@@ -18,9 +22,15 @@
 
 int main(void) {
     mcu_init();
+    buzzer_init();
+
+    MX_ADC1_Init();
+    HAL_ADC_Start(&hadc1);
+
+    uint16_t reading;
 
     for (;;) {
-        led_toggle();
-        mcu_sleep(LED_TOGGLE_DELAY_MS);
+        reading = HAL_ADC_GetValue(&hadc1);
+        buzzer_set_frequency(map(reading, 0, 0x0FFF, MIN_FREQUENCY_HZ, MAX_FREQUENCY_HZ));
     }
 }
